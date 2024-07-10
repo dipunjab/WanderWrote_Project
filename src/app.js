8e0f2dd9c3081dfa5b433e6310e3a9c5fa93719c
Usman <ghaniusman0287@gmail.com>
2024-07-10 17:15:23 +0500

    connected tailwindcss

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import path from "path";
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+    
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express()
 
@@ -10,14 +15,16 @@ app.use(cors({
     credentials: true
 }))
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser())
 
-app.get("/",(req,res)=>{
-    res.send("hey")
-})
-
+app.get('/', (req, res) => {
+    res.render('layout', { title: 'Home' }); // Render index.pug with data
+});
 
 export { app }
