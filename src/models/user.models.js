fe6786660a4d196bef96f1ab6d70e1c640073e60
Usman <ghaniusman0287@gmail.com>
2024-07-15 11:13:09 +0500

    created multer middle ware and cloudinary also setup the user model

---
+++ src/models/user.models.js
@@ -0,0 +1,84 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt, { compare } from "bcrypt"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        about: {
+            type: String,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+            required: true
+        },
+        fullname:{
+                type: String,
+                required: true,
+                index: true
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+        refreshToken: {
+            type: String
+        },
+    }
+    ,{timestamps: true})
+    
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) {
+        return next()
+    }
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+});
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password)
+};
+
+userSchema.methods.generateAccessToken = async function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+};
+
+userSchema.methods.generateRefreshToken = function(password){
+    return jwt.sign({
+        _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+};
+
+export const User = mongoose.model("User",userSchema)
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
-
+import fs from "fs"
+import { ApiError } from './ApiError.js';
 
     // Configuration
     cloudinary.config({ 
@@ -17,7 +18,7 @@ import { v2 as cloudinary } from 'cloudinary';
                 resource_type: "auto"
             })
             //file has been succefully uploaded
-            // console.log("File is uploaded on cloudinary", response.url)
+            console.log("File is uploaded on cloudinary", response.url)
             fs.unlinkSync(localFilePath)
             return response
 
@@ -27,20 +28,5 @@ import { v2 as cloudinary } from 'cloudinary';
         }
     };
 
-    //delete an image
-    const deleteFromCloudinary = async (url,publicId)=>{
-        try {
-            if (!(url||publicId))  throw new ApiError(400,"url or id not found while dleting from cloudinary")
-            
-            const response = await cloudinary.uploader.destroy(publicId,
-                {resource_type: `${url.includes("image") ? "image": "video"}`}
-            )    
-            console.log("Asset deleted from Cloudinary:", response);
-        } catch (error) {
-            console.error("Error deleting asset from Cloudinary:", error);
-            throw new ApiError(500, error?.message || "Server error");
-        }
-    }
 
-
-export {uploadonCloudinary, deleteFromCloudinary}
+export {uploadonCloudinary}
